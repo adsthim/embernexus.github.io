aec96a2e8819f68280b333451ce817a07efd3280
Ember314 <97493200+emberweb@users.noreply.github.com>
2022-02-15 12:15:10 -0500

    Update lock.js

--- lock.js
+++ lock.js
@@ -2,7 +2,7 @@ function submitting(){
     var pinput = document.getElementById("passinput");
     var pvalue = pinput.value;
 
-    if (pvalue == "qunoa"){
+    if (pvalue == process.env.PSECRET){
         localStorage.setItem("allowed","true")
         location.href = "nav.html"
     } else{
