865bc5ae3ffbdbfb45aea6b062cc7d884d5b43c1
Ember314 <97493200+emberweb@users.noreply.github.com>
2022-02-15 11:04:25 -0500

    Create lock.js

---
+++ lock.js
@@ -0,0 +1,11 @@
+function submitting(){
+    var pinput = document.getElementById("passinput");
+    var pvalue = pinput.value;
+
+    if (pvalue == ${{ secrets.PSECRET }}){
+        localStorage.setItem("allowed","true")
+        location.href = "nav.html"
+    } else{
+        alert("Invalid Password");
+    }
+}
