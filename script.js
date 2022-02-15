22f119afe671225e93f67a26d37c75c21b3b915b
Ember314 <97493200+emberweb@users.noreply.github.com>
2022-02-15 11:03:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,4 @@ $(function() {
 	$( ".box" ).draggable();
 });
 
-$(function submitting(){
-    var pinput = document.getElementById("passinput");
-    var pvalue = pinput.value;
 
-    if (pvalue == ${{ secrets.PSECRET }}){
-        localStorage.setItem("allowed","true")
-        location.href = "nav.html"
-    } else{
-        alert("Invalid Password");
-    }
-});
