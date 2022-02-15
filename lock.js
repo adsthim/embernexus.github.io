1dbb35a44f2a2b331782f8ba4a827e60fee59176
Ember314 <97493200+emberweb@users.noreply.github.com>
2022-02-15 14:19:15 -0500

    Update lock.js

--- lock.js
+++ lock.js
@@ -1,11 +1,14 @@
 function submitting(){
     var pinput = document.getElementById("passinput");
     var pvalue = pinput.value;
+    var pinputEncode = btoa(pvalue);
 
-    if (pvalue == process.env.PSECRET){
+
+    if (pinputEncode == "ZW1iZXJpc2tpbmRhY29vbDMxNA=="){
         localStorage.setItem("allowed","true")
         location.href = "nav.html"
     } else{
         alert("Invalid Password");
     }
 }
+
